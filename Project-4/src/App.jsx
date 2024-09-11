c21201b19c006bbe75d9b88364c8e281e8919b02
Umapathi2004 <umapathi1014@gmail.com>
2024-09-11 22:24:43 +0530

    changed

--- Project-4/src/App.jsx
+++ Project-4/src/App.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const[clickPro,SetClickpro] = useState(false);
+  const[clickAp,SetClickap] = useState(false);
+  const[clickSa,SetClickSa] = useState(false);
+  const clickProfile = () =>{
+    SetClickap(false);
+    SetClickSa(false);
+    SetClickpro(true);
+  }
+  const clickApp = () =>{
+    SetClickap(true);
+    SetClickSa(false);
+    SetClickpro(false);
+  }
+  const clickSave = () =>{
+    SetClickap(false);
+    SetClickSa(true);
+    SetClickpro(false);
+  }
+  useEffect(()=>{
+    clickApp();
+  },[])
   return (
     <>
     <div className="container_one">
@@ -26,6 +47,30 @@ const App = () => {
          <div className="edit"><i class="fa-solid fa-pen"></i></div>
       </div>
     </div>
+    <div className="conatiner_three">
+      <div className="menus">
+        <div className={`menu ${clickPro && "clicked"}`} onClick={()=>clickProfile()}>Profile</div>
+        <div className={`menu ${clickAp && "clicked"}`} onClick={()=>clickApp()}>Applications</div>
+        <div className={`menu ${clickSa && "clicked"}`} onClick={()=>clickSave()}>Save Jobs</div>
+      </div>
+      <div className="main_content">
+        <div className="detials">
+          <div className="time">Full time</div>
+          <div className="role">Web Developers</div>
+          <div className="location">Tamil Nadu, Tamil Nadu</div>
+          <div className="expreiance">0-1year</div>
+          <div className="discrption">Develop new components based on design specs. Work independently and write maintainable code. Understand existing code and suggest optimization in ter...</div>
+        </div>
+        <div className="status">
+          <div className="app_status">Screening</div>
+          <div className="app_date"><span className='small'>Applied on</span><span>17/07/2024</span></div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div className="powered">Powered By</div>
+      <div className="footer_logo"></div>
+    </footer>
     </>
   )
 }
